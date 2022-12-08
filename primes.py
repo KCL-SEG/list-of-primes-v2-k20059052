3efdaed8ab941f6b6781ebf4544bd16bca90be58
k20059052 <101188329+k20059052@users.noreply.github.com>
2022-12-08 12:37:31 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    count = 2
+    while number_of_primes != 0:
+        prime = True
+
+        for i in range(2, count):
+            if count % i == 0:
+                prime = False
+
+        if prime:
+            list.append(count)
+            number_of_primes -= 1
+
+        count += 1
+
     return list
