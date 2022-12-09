ec74c704cd33e778bb7e432e9d69dbe3491d1ae5
k20059052 <101188329+k20059052@users.noreply.github.com>
2022-12-09 00:08:19 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,24 +1,25 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
 
 def primes(number_of_primes):
-    list = []
-
     if number_of_primes <= 0:
         raise ValueError
-
-    counter = 2
-    while number_of_primes != 0:
-        prime = True
-
-        for i in range(2, counter):
-            if counter % i == 0:
+    
+    list = [] 
+    prime = True 
+    base_number = 2
+    while len(list) != number_of_primes:
+      
+        for divisor in range(2,base_number):
+            prime = True
+            if(base_number % divisor) == 0:
                 prime = False
-
-        if prime:
-            list.append(counter)
-            number_of_primes -= 1
-
-        counter += 1
-
+                break
+            
+        if prime == True:
+            list.append(base_number)
+    
+        
+        
+        base_number +=1
+    
     return list
